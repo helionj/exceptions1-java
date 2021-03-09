e5bd86a2870d60fbd48e81df10daedb68e93cf80
helionporto <helionporto@hotmail.com>
2021-03-08 20:30:47 -0400

    Bad solution

--- src/com/helion/application/Program.java
+++ src/com/helion/application/Program.java
@@ -35,14 +35,12 @@ public class Program {
 			System.out.print("Check-out date (DD/MM/YYYY) : ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Resrevation dates for update must be future");
-			}else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}else {
-				reservation.updateDates(checkIn, checkOut);
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error == null) {
+				
 				System.out.println("Reservation: "+reservation);
+			}else {
+				System.out.println(error);
 			}
 			
 		}
